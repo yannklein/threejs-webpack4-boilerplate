49213731b7e4f12a9a56dbf4729415be5e9bd6ff
Yann Klein <yann.klein@me.com>
2020-04-13 15:45:58 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+
+import './fonts/libre-baskerville-v5-latin-regular.woff';
+import './fonts/libre-baskerville-v5-latin-regular.woff2';
+
+import './index.html';
+import './index.scss';
+import './scripts/script';
+
+OfflinePluginRuntime.install();
